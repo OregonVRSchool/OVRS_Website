9478ef9306800d00d00b17c472ba6e15fa368e9f
unknown <oregonvrschool@gmail.com>
2018-04-12 11:44:50 -0700

    page.blade.php is now dynamic

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.category')
 
 @section('content')
-
-    <div id="{!! $title !!}">
-    	<div class="title">
-    		{!! $title !!}
-    	</div>
-    	<div class="content">
-    		{!! $content !!}
-    	</div>
-    </div>
+	@if ($title === "index")
+	    <div id="{!! $category_title !!}">
+	    	<div class="title">
+	    		{!! $category_title !!}
+	    	
+	@else
+		<div id="{!! $title !!}">
+	    	<div class="title">
+	    		{!! $title !!}
+	@endif
+	
+			</div>
+			<div class="content">
+	    		{!! $content !!}
+	    	</div>
+	    </div>
      
 @endsection
