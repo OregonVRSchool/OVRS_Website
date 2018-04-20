c6e25ac3bdb8cd38aa11c7063cf6bec04c9fb0c1
unknown <oregonvrschool@gmail.com>
2018-04-20 10:18:58 -0700

    polishing

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,21 +1,23 @@
-@extends('layouts.category')
+@extends('layouts.app')
 
 @section('content')
-	@if ($page->title === "index")
-	    <div id="{!! $page->category->title !!}">
-	    	<div class="title">
-	    		{!! $page->category->title !!}
-	    	
-	@else
-		<div id="{!! $page->title !!}">
-	    	<div class="title">
-	    		{!! $page->title !!}
-	@endif
-	
-			</div>
-			<div class="content">
-	    		{!! $page->content !!}
-	    	</div>
-	    </div>
+	<div id="page"> 
+		@if ($page->title === "index")
+		    <div id="{!! $page->category->title !!}">
+		    	<h1 class="title">
+		    		{!! $page->category->title !!}
+		    	
+		@else
+			<div id="{!! $page->title !!}">
+		    	<h1 class="title">
+		    		{!! $page->title !!}
+		@endif
+		
+				</h1>
+				<div class="content">
+		    		{!! $page->content !!}
+		    	</div>
+		    </div>
+	</div>
      
 @endsection
