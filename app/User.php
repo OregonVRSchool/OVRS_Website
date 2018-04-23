5b37cef7f1cc802d66a6beac11cf12f3e5b7d598
unknown <oregonvrschool@gmail.com>
2018-04-23 10:01:21 -0700

    permissions table migration

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function permission()
+    {
+        return $this->belongsTo('App\Permissions');
+    }
 }
