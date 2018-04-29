4597ee3c18c1951ab7f8b61b577ad357f946d6d9
Andrew Earls <andrewearls@rocketmail.com>
2018-04-28 20:45:54 -0700

    fixing main height

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn">
                                     {{ __('Login') }}
                                 </button>
