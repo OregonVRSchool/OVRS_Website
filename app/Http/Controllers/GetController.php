b988d18362afe10dc776311e7b13a4645b14dd95
unknown <oregonvrschool@gmail.com>
2018-05-25 11:33:38 -0700

    student previous school form polished

--- app/Http/Controllers/GetController.php
+++ app/Http/Controllers/GetController.php
@@ -49,6 +49,6 @@ class GetController extends BaseController
 
     public function test()
     {
-        return view('partials.forms.applications.student.interests');
+        return view('partials.forms.applications.student.schools');
     }
 }
--- resources/views/partials/forms/applications/student/schools.blade.php
+++ resources/views/partials/forms/applications/student/schools.blade.php
@@ -9,18 +9,32 @@
 @endsection
 
 @section('form-content')
+
 	{!! Form::label('schoolName', 'School Name') !!}
 	{!! Form::text ('schoolName') !!}	
-	{!! Form::label('address', 'Address') !!}
-	{!! Form::text ('address') !!}
-	{!! Form::label('phone', 'Phone') !!}
-	{!! Form::text ('phone') !!}
-	{!! Form::label('fromDate', 'From Date') !!}
-	{!! Form::text ('fromDate') !!}
-	{!! Form::label('toDate', 'To Date') !!}
-	{!! Form::text ('toDate') !!}
-	{!! Form::label('gradeCompleted', 'Grade Completed') !!}
-	{!! Form::text ('gradeCompleted') !!}
+	<div class="subsection-header">
+		Location
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			{!! Form::label('address', 'Address') !!}
+			{!! Form::text ('address') !!}
+			{!! Form::label('phone', 'Phone Number') !!}
+			{!! Form::text ('phone') !!}
+		</div>
+		<div class="col-md-6">
+			{!! Form::label('fromDate', 'From Date') !!}
+			{!! Form::text ('fromDate') !!}
+			{!! Form::label('toDate', 'To Date') !!}
+			{!! Form::text ('toDate') !!}
+			{!! Form::label('gradeCompleted', 'Highest Grade Completed') !!}
+		{!! Form::text ('gradeCompleted') !!}
+		</div>
+	</div>
+
+	<div class="subsection-header">
+		Teacher References
+	</div>	
 	
 	{!! Form::label('mathTeacher', 'Name of students current Math teacher') !!}
 	{!! Form::text ('mathTeacher') !!}
@@ -29,13 +43,21 @@
 	{!! Form::label('computerTeacher', 'Name of students current Computer teacher') !!}
 	{!! Form::text ('computerTeacher') !!}
 
+	<div class="subsection-header"> 
+		Student Conduct
+	</div>
+
 	{!! Form::label('expelled', 'Has the student ever been expelled') !!}
-	{!! Form::text ('expelled') !!}
+	{!! Form::textarea ('expelled', null, ['size' => '50x3']) !!}
 	{!! Form::label('suspended', 'Has the student ever been suspended') !!}
-	{!! Form::text ('suspended') !!}
+	{!! Form::textarea ('suspended', null, ['size' => '50x3']) !!}
 	{!! Form::label('skippedGrade', 'Has the student ever skipped a grade') !!}
-	{!! Form::text ('skippedGrade') !!}
+	{!! Form::textarea ('skippedGrade', null, ['size' => '50x3']) !!}
+
+	<div class="subsection-header">
+		Educational Plans
+	</div>
 
 	{!! Form::label('iep', 'Does the student have an (IEP) or 504 plan') !!}
-	{!! Form::text ('iep') !!}
+	{!! Form::select ('iep', array('Y' => 'Yes', 'N' => 'No'), 'N') !!}
 @endsection
