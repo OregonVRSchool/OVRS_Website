28761e377820a74bee4a9b27863b65dffd60e524
unknown <oregonvrschool@gmail.com>
2018-05-29 11:41:47 -0700

    edit application sets session variables

--- app/Http/Controllers/StudentApplicationController.php
+++ app/Http/Controllers/StudentApplicationController.php
@@ -16,15 +16,15 @@ class StudentApplicationController extends BaseController
 
     public function newStudent(Request $request)
     {
-        $student = new Application;
-        $student->user_id = Auth::user()->id;
-        $student->first_name = $request['firstName'];
-        $student->last_name = $request['lastName'];
-        $student->year = $request['year'];
-        $student->grade = $request['grade'];
-        $student->save();
+        $application = new Application;
+        $application->user_id = Auth::user()->id;
+        $application->first_name = $request['firstName'];
+        $application->last_name = $request['lastName'];
+        $application->year = $request['year'];
+        $application->grade = $request['grade'];
+        $application->save();
 
-        $request->session()->put('applicant', ['id' => $student->id, 'firstName' => $student->first_name]);
+        $request->session()->put('applicant', ['id' => $application->id, 'firstName' => $application->first_name]);
 
         return redirect()->route('information.student.application');
     }
@@ -32,6 +32,8 @@ class StudentApplicationController extends BaseController
     public function editStudent($id)
     {
         $application = Auth::user()->applications->find($id);
+
+        $request->session()->put('applicant', ['id' => $application->id, 'firstName' => $application->first_name]);
 
         return redirect()->route('application-student', ['application' => $application]);
     }
