cfa2f745983af8bdf263f9126d530eeb4f9eb546
unknown <oregonvrschool@gmail.com>
2018-07-25 17:00:28 -0700

    delete file now working

--- resources/views/partials/forms/applications/parts/fileUpload.blade.php
+++ resources/views/partials/forms/applications/parts/fileUpload.blade.php
@@ -6,7 +6,7 @@
 		
 		<div class="col-4">
 			<img src="data:image/jpeg;base64,{{ (isset($page->file->content)) ?$page->file->content : null }}">
-			<div>{{ (isset($page->file->name)) ?$page->file->name : null }}</div>
+			<div>{!! (isset($page->file->name)) ?$page->file->name : null !!}</div>
 			<button type="button" onclick="deleteFile()">Delete</button>
 		</div>
 		
@@ -18,8 +18,8 @@
 		document.getElementById("display-upload").style.display = "none";
 		$.ajax({
 			type: "POST",
-			url: '{{ $page->id }}/delete/file',
-			data: {id: {{ (isset($page->file->id)) ?$page->file->id : null }} },
+			url: '{!! session("applicant")["id"] !!}/delete/file',
+			data: {id: {!! (isset($page->file->id)) ?$page->file->id : null !!} },
 			headers: {
 		        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 		    },
